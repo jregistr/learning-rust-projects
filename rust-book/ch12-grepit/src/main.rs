4cecf88a055356e77dd42911ae866ab76fbb56e2
Jeff Registre <jeffreyregistre@gmail.com>
2021-09-29 09:51:48 -0400

    wip

--- rust-book/ch12-grepit/src/main.rs
+++ rust-book/ch12-grepit/src/main.rs
@@ -1,11 +1,35 @@
-use std::{env, fs};
+use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = (&args[0], &args[1]);
+    let config = Config::from_args(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem processing args: {}", err);
+            process::exit(1)
+        });
+    run(config);
+}
 
-    let file_str = fs::read_to_string(&filename)
-        .expect(format!("No file found at {}", &filename).as_str());
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let file_str = fs::read_to_string(&file_name)?;
 
     println!("{}", file_str);
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Config {
+    file_name: String,
+    query: String,
+}
+
+impl Config {
+    fn from_args(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough args");
+        }
+        let (query, fnn) = (&args[1], &args[2]);
+        Ok(Config { file_name: fnn.clone(), query: query.clone() })
+    }
 }
